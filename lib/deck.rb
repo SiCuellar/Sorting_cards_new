159081d62da2b15b251ef11e340a06a4001a347f
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-08-21 08:27:06 -0600

    working on fucntioning sort method

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,4 +10,33 @@ class Deck
   def count
     cards.count
   end
+
+  def sort
+    value_rank = {
+      "2": 1,
+      "3": 2,
+      "4": 3,
+      "5": 4,
+      "6": 5,
+      "7": 6,
+      "8": 7,
+      "9": 8,
+      "10": 9,
+      "Jack": 10,
+      "Queen": 11,
+      "King": 12,
+      "Ace": 13,
+      "Clubs": 0.1,
+      "Diamonds": 0.2,
+      "Hearts": 0.3,
+      "Spades": 0.4
+    }
+
+    
+
+
+
+
+
+  end
 end
